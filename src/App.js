f6fba455fe21f833dd85d8d4cc370921ab3ad6ed
Ankita <ankita24rose@gmail.com>
2026-04-01 08:57:03 +0530

    app.js file has been modified

--- src/App.js
+++ src/App.js
@@ -7,34 +7,55 @@ import UpdateProduct from './components/UpdateProduct';
 import About from './components/About';
 
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route
+    BrowserRouter as Router,
+    Routes,
+    Route
 } from 'react-router-dom';
 
 
 
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar title="IMS" about="About"></Navbar>
+    return ( <
+        div className = "App" >
+        <
+        Navbar title = "IMS"
+        about = "About" > < /Navbar>
 
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/insertproduct" element={<InsertProduct />} />
-          <Route path="/updateproduct/:id" element={<UpdateProduct />} />
-          <Route path="/about" element={<About />} />
+        <
+        Router >
+        <
+        Routes >
+        <
+        Route exact path = "/"
+        element = { < Home / > }
+        /> <
+        Route path = "/products"
+        element = { < Products / > }
+        /> <
+        Route path = "/insertproduct"
+        element = { < InsertProduct / > }
+        /> <
+        Route path = "/updateproduct/:id"
+        element = { < UpdateProduct / > }
+        /> <
+        Route path = "/about"
+        element = { < About / > }
+        /> <
+        Route path = "/projects"
+        element = { < About / > }
+        />
 
-        </Routes>
+        <
+        /Routes>
 
-      </Router>
+        <
+        /Router>
 
 
-    </div>
-  );
+        <
+        /div>
+    );
 }
 
 export default App;
